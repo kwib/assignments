a37794f2f618cead5529f560a506ea54d0e4bcb2
yongsheng <leon800219@gmail.com>
2018-05-04 19:02:12 +0800

    initial check in

--- assignment.js
+++ assignment.js
@@ -15,33 +15,56 @@ $(function () {
   }
 
   function processData(jsonData) {
+    var filterPet = "Cat";
     var catsMaleOwner = [];
     var catsFemaleOwner = [];
     var petsMaleElement = document.getElementById("petsMale");
     var petsFemaleElement = document.getElementById("petsFemale");
 
-      for (var i = 0; i < jsonData.length; i++) {
-          if (jsonData[i]["gender"] === "Male") {
-              catsMaleOwner.push.apply(catsMaleOwner, jsonData[i]["pets"]);
-          } else {
-              catsFemaleOwner.push.apply(catsFemaleOwner, jsonData[i]["pets"])
-          }
+    // Filter
+    for (let i = 0; i < jsonData.length; i++) {
+      if (jsonData[i].gender === "Male") {
+          catsMaleOwner.push.apply(catsMaleOwner, jsonData[i].pets);
+      } else {
+          catsFemaleOwner.push.apply(catsFemaleOwner, jsonData[i].pets)
       }
+    }
 
-      $.each(catsFemaleOwner, function (index, item) {
-          var li = document.createElement("li");
+    catsMaleOwner = $.grep(catsMaleOwner, function(v) {
+      return v.type === filterPet;
+    });
 
-          li.textContent = item && item["name"] || '';
-          petsFemaleElement.appendChild(li);
-      });
+    catsFemaleOwner = $.grep(catsFemaleOwner, function(v) {
+      return v.type === filterPet;
+    });
 
+    // Sort
+    catsMaleOwner = catsMaleOwner.sort(function(a, b){
+      if(a.name < b.name) return -1;
+      if(a.name > b.name) return 1;
+      return 0;
+    })    
 
-      $.each(catsMaleOwner, function (index, item) {
-          var li = document.createElement("li");
+    catsFemaleOwner = catsFemaleOwner.sort(function(a, b){
+      if(a.name < b.name) return -1;
+      if(a.name > b.name) return 1;
+      return 0;
+    }) 
+      
+    // Render
+    $.each(catsFemaleOwner, function (index, item) {
+      var li = document.createElement("li");
 
-          li.textContent = item && item["name"] || '';
-          petsMaleElement.appendChild(li);
-      });
+      li.textContent = item && item["name"] || '';
+      petsFemaleElement.appendChild(li);
+    });
+
+    $.each(catsMaleOwner, function (index, item) {
+      var li = document.createElement("li");
+
+      li.textContent = item && item["name"] || '';
+      petsMaleElement.appendChild(li);
+    });
   }
 
   fetchData();
